91bcb253dfbbf13bcbcb7f45cb3dd226120efe35
iplai <365141210@qq.com>
2023-06-04 08:45:33 +0800

    update tulsi couple

--- my_examples/tulsi_couple.py
+++ my_examples/tulsi_couple.py
@@ -2,7 +2,7 @@ from pynodes import *
 
 
 @tree
-def tulsi_couple(count: Integer = (3, 1, 8)):
+def tulsi_couple(count: Integer = (5, 1, 8)):
 
     line = CurveLine((0, -3, 0), (0, 3, 0))
 
@@ -16,9 +16,9 @@ def tulsi_couple(count: Integer = (3, 1, 8)):
 
         distance = lines.to_mesh().proximity("EDGES").distance
 
-    with frame("Set grid position bt distance"):
+    with frame("Set grid position by distance"):
 
-        plane = MeshGrid(20, 20, 512, 512).mesh
+        plane = MeshGrid(20, 20, 200, 200).mesh
 
         plane[distance < 1].set_position(offset=-plane.nornal * (1 - distance).float_curve(points=[(0, 0), (0.05, 0.03), (0.5, 0.5, "auto_clamped"), (1, 0.5)]))
 
@@ -28,7 +28,7 @@ def tulsi_couple(count: Integer = (3, 1, 8)):
 
         sphere = MeshIcoSphere(radius=0.2, subdivisions=3).mesh
 
-        t = Float.sin(SceneTime().seconds * 2).map_range(-1, 1, 0, 1)
+        t = Float.sin(SceneTime().seconds * 4 + (lines.index / count) * pi).map_range(-1, 1, 0, 1)
 
         spheres = sphere.on_points(lines.trim_factor(start=t)[lines.endpoint_selection(start_size=1, end_size=0)])
 
